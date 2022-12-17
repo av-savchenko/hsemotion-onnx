ad83dc1342d5145d3118530567443cde3f9db80e
avsavchenko <andrey.v.savchenko@gmail.com>
2022-12-17 14:36:06 +0300

    Changed version

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ requirements = [
 ]
 setup(
     name='hsemotion-onnx',
-    version='0.3.0',
+    version='0.3.1',
     license='Apache-2.0',
     author="Andrey Savchenko",
     author_email='andrey.v.savchenko@gmail.com',
     packages=find_packages('.'),
-    download_url = 'https://github.com/HSE-asavchenko/hsemotion-onnx/archive/v0.3.0.tar.gz',
+    download_url = 'https://github.com/HSE-asavchenko/hsemotion-onnx/archive/v0.3.1.tar.gz',
     url='https://github.com/HSE-asavchenko/face-emotion-recognition',
     description='HSEmotionONNX Python Library for Facial Emotion Recognition',
     keywords=['face expression recognition', 'emotion analysis', 'facial expressions'],
